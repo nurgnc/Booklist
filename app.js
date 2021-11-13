e6029d566cb69a8f7e1305bf0e85ea32894960bc
nurgnc <gencbedriyenur@gmail.com>
2021-11-13 15:55:36 +0300

    completed UI design

---
+++ app.js
@@ -0,0 +1,18 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+//UI Class: Handle UI Tasks
+
+//Store Class: Handles Storage
+
+//Event: Display Books
+
+//Event: Add a Book
+
+//Event: Remove a Book
